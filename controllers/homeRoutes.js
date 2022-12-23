2d048de5303f8d0d31dc9155c830dcb7635e89c9
Ghazala Ahmed <ghazala599@gmail.com>
2022-12-23 10:25:41 -0800

    fixed errors in files

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,5 +1,6 @@
+const sequelize = require("../config/connection");
 const router = require("express").Router();
-const { Post, Comment, User } = require("../models");
+const { Post, Comment, User } = require("../models/");
 const withAuth = require("../utils/auth");
 
 router.get("/", async (req, res) => {
@@ -9,14 +10,13 @@ router.get("/", async (req, res) => {
     });
 
     const posts = postData.map((post) => post.get({ plain: true }));
-
-    res.render("posts-comments", { post, loggedIn: req.session.loggedIn });
+    res.render("homepage", { posts, loggedIn: req.session.loggedIn });
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-router.get("/post/:id", async (req, res) => {
+router.get("/post/:id", withAuth, async (req, res) => {
   try {
     const postData = await Post.findOne({
       where: { id: req.params.id },
@@ -31,6 +31,7 @@ router.get("/post/:id", async (req, res) => {
 
     if (postData) {
       const post = postData.get({ plain: true });
+
       console.log(post);
       res.render("single-post", { post, loggedIn: req.session.loggedIn });
     } else {
